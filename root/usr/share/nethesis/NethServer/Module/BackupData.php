90ba25ea4897bade8d61581f224ee09109701245
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-01-30 11:18:30 +0100

    UI: fix syntax in regexp validator

--- root/usr/share/nethesis/NethServer/Module/BackupData.php
+++ root/usr/share/nethesis/NethServer/Module/BackupData.php
@@ -48,7 +48,7 @@ class BackupData extends \Nethgui\Controller\AbstractController
     {
         parent::initialize();
         $this->declareParameter('status', Validate::SERVICESTATUS, array('configuration', 'backup-data', ''));
-        $this->declareParameter('BackupTime', $this->createValidator()->regexp('^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])$'), array('configuration', 'backup-data', 'BackupTime'));
+        $this->declareParameter('BackupTime', $this->createValidator()->regexp('/^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])$/'), array('configuration', 'backup-data', 'BackupTime'));
         $this->declareParameter('Type', $this->createValidator()->memberOf(array('full','incremental')), array('configuration', 'backup-data', 'Type'));
         $this->declareParameter('ForceFull',Validate::SERVICESTATUS, array('configuration', 'backup-data', 'FaxName'));
         $this->declareParameter('FullDay', $this->createValidator()->integer()->greatThan(-1)->lessThan(7), array('configuration', 'backup-data', 'FullDay'));
