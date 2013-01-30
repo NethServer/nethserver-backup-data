216152e1448791dbce26a863897ee07643ba3f6a
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-01-30 10:41:51 +0100

    UI: add italian and english translations

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_BackupData.php
@@ -0,0 +1,38 @@
+<?php 
+
+/* NethServer_Module_BackupData translation, language: en */
+
+$L['0_label'] = 'Sunday';
+$L['1_label'] = 'Monday';
+$L['2_label'] = 'Tuesday';
+$L['3_label'] = 'Wednesday';
+$L['4_label'] = 'Thursday';
+$L['5_label'] = 'Friday';
+$L['6_label'] = 'Saturday';
+$L['BackupData_Description'] = 'Manage backup of data files';
+$L['BackupData_Destination_Title'] = 'Destination';
+$L['BackupData_General_Title'] = 'General';
+$L['BackupData_Tags'] = 'backup data full incremental';
+$L['BackupData_Title'] = 'Backup';
+$L['BackupDataType_label'] = 'Type';
+$L['BackupTime_label'] = 'Schedule time';
+$L['cifs_label'] = 'CIFS';
+$L['ForceFull_label'] = 'Force full backup when needed';
+$L['FullDay_label'] = 'Full backup will be on';
+$L['full_label'] = 'Full';
+$L['incremental_label'] = 'Incremental';
+$L['NFSHost_label'] = 'Host';
+$L['nfs_label'] = 'NFS';
+$L['NFSShare_label'] = 'Share';
+$L['SMBHost_label'] = 'Server';
+$L['SMBLogin_label'] = 'Username';
+$L['SMBPassword_label'] = 'Password';
+$L['SMBShare_label'] = 'Share';
+$L['status_label'] = 'Enable automatic backup';
+$L['Type_full_label'] = 'Full';
+$L['Type_incremental_label'] = 'Incremental';
+$L['usb_label'] = 'USB';
+$L['USBLabel_label'] = 'Disk';
+$L['VFSType_cifs_label'] = 'Windows share (CIFS)';
+$L['VFSType_nfs_label'] = 'NFS share';
+$L['VFSType_usb_label'] = 'USB disk';
