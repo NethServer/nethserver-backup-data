ab2f1c85e9d3e786144d3d5f1dc1b5cd9250022f
Ana Fernandez <anuska_716@hotmail.com>
2014-05-06 12:48:02 +0200

    Web interface: add Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_BackupData.php
@@ -0,0 +1,52 @@
+<?php 
+
+/* NethServer_Module_BackupData translation, language: es */
+
+$L['0_label'] = 'Domingo';
+$L['1_label'] = 'Lunes';
+$L['2_label'] = 'Martes';
+$L['3_label'] = 'Miercoles';
+$L['4_label'] = 'Jueves';
+$L['5_label'] = 'Viernes';
+$L['6_label'] = 'Sábado';
+$L['7D_label'] = '7 días';
+$L['30D_label'] = '30 días';
+$L['60D_label'] = '60 días';
+$L['BackupData_Description'] = 'Administrar copias de seguridad de archivos de datos';
+$L['BackupData_Destination_Title'] = 'Destino';
+$L['BackupData_General_Title'] = 'General';
+$L['BackupData_Notification_Title'] = 'Notificación';
+$L['BackupData_Tags'] = 'datos de copia de seguridad incremental completa';
+$L['BackupData_Title'] = 'Copia de seguridad';
+$L['BackupDataType_label'] = 'Tipo';
+$L['BackupTime_label'] = 'Programe tiempo';
+$L['CleanupOlderThan_label'] = 'Eliminar las copias de seguridad anteriores a:';
+$L['cifs_label'] = 'CIFS';
+$L['FullDay_label'] = 'Copia de seguridad completa estará en';
+$L['full_label'] = 'Completo';
+$L['incremental_label'] = 'Incremental';
+$L['NFSHost_label'] = 'Host';
+$L['never_label'] = 'No';
+$L['nfs_label'] = 'NFS';
+$L['notify_label'] = 'Notificar';
+$L['notify_always_label'] = 'Siempre';
+$L['notify_error_label'] = 'En caso de error';
+$L['notify_never_label'] = 'No';
+$L['notifyToCustom_label'] = 'Dirección';
+$L['notifyToType_admin_label'] = 'Sistema administrador';
+$L['notifyToType_custom_label'] = 'Correo electrónico personalizado';
+$L['notifyTo_label'] = 'Enviar notificación a';
+$L['NFSShare_label'] = 'Compartir';
+$L['RetentionPolicy_label'] = 'Política de retención';
+$L['SMBHost_label'] = 'Servidor';
+$L['SMBLogin_label'] = 'Nombre de usuario';
+$L['SMBPassword_label'] = 'Contraseña';
+$L['SMBShare_label'] = 'Compartir';
+$L['status_label'] = 'Habilitar copia de seguridad automática';
+$L['Type_full_label'] = 'Completo';
+$L['Type_incremental_label'] = 'Incremental';
+$L['usb_label'] = 'USB';
+$L['USBLabel_label'] = 'Filesystem label';
+$L['VFSType_cifs_label'] = 'Cuota de Windows (CIFS)';
+$L['VFSType_nfs_label'] = 'Compartir NFS';
+$L['VFSType_usb_label'] = 'Disco USB';
---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_Dashboard_SystemStatus_Backup.php
@@ -0,0 +1,20 @@
+<?php 
+
+/* Language/en/NethServer_Module_Dashboard_SystemStatus_Backup translation, language: en */
+
+$L['backup_title'] = 'Copia de seguridad';
+$L['status_label'] = 'Estado';
+$L['time_label'] = 'Lista en';
+$L['vfs_label'] = 'Destino';
+$L['type_label'] = 'Tipo';
+$L['result_label'] = 'Resultado';
+$L['start_label'] = 'Hora de inicio';
+$L['end_label'] = 'Tiempo final';
+$L['enabled'] = 'Habilitado';
+$L['disabled'] = 'Deshabilitado';
+$L['incremental'] = 'Incremental';
+$L['full'] = 'Completo';
+$L['SUCCESS'] = 'ÉXITO';
+$L['ERROR'] = 'ERROR';
+$L['backup_not_configured'] = 'La copia de seguridad no está configurada';
+$L['last_backup'] = 'Última copia de seguridad';
