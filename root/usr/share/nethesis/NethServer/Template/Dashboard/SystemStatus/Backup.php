ac9617f12bfa7a4ef0f9930e41bffe2966143253
dnutan <dnutan@users.noreply.github.com>
2017-03-07 09:39:28 +0100

    Basic webDAV support for backups + Backup storage stats (#9)
    * Add WebDAV support ad backup backend
    * Add backup storage stats
    * Hide password fields inside the UI (thanks to @robb)
    
    NethServer/dev#5235

--- root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus/Backup.php
+++ root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus/Backup.php
@@ -1,5 +1,16 @@
 <?php
 
+function formatSize($size)
+{
+    $units = array(' B', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB');
+    $u = count($units)-1;
+    for ($i = 0; ($size >= 1024 && $i < $u); $i ++ ) {
+        $size /= 1024;
+    }
+    return round($size, 2) . $units[$i];
+}
+
+
 echo "<div class='dashboard-item'>";
 echo $view->header()->setAttribute('template',$T('backup_title'));
 if (!$view['backup_status']['vfs']) {
@@ -14,6 +25,13 @@ if (!$view['backup_status']['vfs']) {
     echo "<dt>".$T('result_label')."</dt><dd class='backup-".strtolower($view['backup_result'])."'>"; echo $T($view['backup_result']); echo "</dd>";
     echo "<dt>".$T('start_label')."</dt><dd>"; echo date("Y-m-d H:i",$view['backup_start']); echo "</dd>";
     echo "<dt>".$T('end_label')."</dt><dd>"; echo date("Y-m-d H:i",$view['backup_end']); echo "</dd>";
+    if ( strlen($view['backup_size']) && strlen($view['backup_used']) && strlen($view['backup_avail']) ) {
+        echo "<dt>".$T('usage_label')."</dt><dd>".formatSize($view['backup_used']*1024) ." / " . formatSize($view['backup_size']*1024)."</dd>";
+        echo "<dt>".$T('avail_label')."</dt><dd>".formatSize($view['backup_avail']*1024)."</dd>";
+        echo "<div id='backup_plot' value='{$view['backup_used']}' max='{$view['backup_size']}'><div id='backup_label' class='progress-label'></div></div>";
+    } else {
+	    echo "<dt>".$T('storage_stats')."</dt><dd>".$T('unknown')."</dd>";
+    }
     echo "</dl>";
 }
 echo "</div>";
@@ -35,5 +53,56 @@ $view->includeCss("
         color: red;
         font-weight: bold;
     }
+    .dashboard-item .ui-progressbar {
+        position: relative;
+    }
 
+");
+
+$view->includeJavascript("
+(function ( $ ) {
+
+    function refreshMasonry() {
+        if ($(window).width() > 500) {
+            $('#Dashboard_SystemStatus').masonry({
+                itemSelector: '.dashboard-item',
+                isAnimated: false,
+            });
+        }
+    }
+    function updateProgress(name, max, value, colorize) {
+         p = value/max*100;
+         $('#'+name+'_plot').progressbar( { value: p });
+         $('#'+name+'_label').text(p.toPrecision(3)+'%');
+         progressbarValue = $('#'+name+'_plot').find('.ui-progressbar-value');
+
+         if (!colorize) {
+             progressbarValue.css({ 'background': '#eee' });
+             return;
+         }
+         color = 'green';
+         if (p < 70) {
+             color = 'green';
+         } else if (p >=70 && p<=80) {
+             color = 'yellow';
+         } else if (p>80 && p<90) {
+             color = 'orange';
+         } else {
+             color = 'red';
+         }
+         progressbarValue.css({ 'background': color });
+
+    }
+
+
+    $(document).ready(function() {
+        updateProgress('backup', $('#backup_plot').attr('max'), $('#backup_plot').attr('value'), 1);
+        setTimeout(refreshMasonry,100);
+        $( '#Dashboard' ).bind( 'tabsshow', function(event, ui) {
+            if (ui.panel.id == 'Dashboard_SystemStatus') {
+                refreshMasonry();
+            }
+	});
+    });
+} ( jQuery ));
 ");
